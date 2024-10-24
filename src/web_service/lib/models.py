48a29b61049592d9136bb1b0a6d173eb08c71534
Cubestormer-IV <tanmay@gmail.com>
2024-10-24 20:15:52 +0200

    Minor doctring removals

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,1 +1,15 @@
-# Pydantic models for the web service
+from pydantic import BaseModel
+
+
+class InputSchema(BaseModel):
+    Sex: str
+    Length: float
+    Diameter: float
+    Height: float
+    Whole_weight: float
+    Shucked_weight: float
+    Viscera_weight: float
+    Shell_weight: float
+
+class PredictionResponseSchema(BaseModel):
+    predicted_age: float
--- src/web_service/main.py
+++ src/web_service/main.py
@@ -13,6 +13,6 @@ def home() -> dict:
     return {"health_check": "App up and running!"}
 
 
-@app.post("/predict", response_model="InsertHereAPydanticClass", status_code=201)
-def predict(payload: "InsertHereAPydanticClass") -> dict:
+@app.post("/predict", response_model=PredictionResponseSchema, status_code=201)
+def predict(payload: InputSchema) -> dict:
     # TODO: complete and replace the "InsertHereAPydanticClass" with the correct Pydantic classes defined in web_service/lib/models.py
