e5ceca19cbb896556eef8f74eabd0ba9cf702647
qly0923 <qiling.zhu@polytechnique.edu>
2024-10-24 20:00:54 +0200

    added new file app_config.py

---
+++ src/web_service/app_config.py
@@ -0,0 +1,12 @@
+# MODELS
+MODEL_VERSION = "0.0.1"
+#PATH_TO_PREPROCESSOR = f"local_models/dv__v{MODEL_VERSION}.pkl"
+#PATH_TO_MODEL = f"local_models/model__v{MODEL_VERSION}.pkl"
+
+
+# MISC
+APP_TITLE = "AbaloneAgePredictionApp"
+APP_DESCRIPTION = (
+    "A simple API to predict the age of abalone based on the characteristics (weight, height, etc.) provided "
+)
+APP_VERSION = "0.0.1"
