a00ce6b850314f41e8858dc3431b950b72be9e82
elisebaratt <71435423+elisebaratt@users.noreply.github.com>
2024-10-24 15:16:57 +0200

    Added docstirngs and type hinting for better clarity

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,33 +1,60 @@
-# Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
 import os
 import pickle
 from functools import lru_cache
 from loguru import logger
 
+def pickle_object(obj: object, filename: str, folder_path: str = '../web_service/local_objects') -> None:
+    """
+    Pickles and saves the given object to a specified folder.
 
-def pickle_object(obj, filename, folder_path='../web_service/local_objects'):
-    # Ensure the folder exists
+    Args:
+        obj (object): The object to pickle.
+        filename (str): The name of the file to save the object to, without extension.
+        folder_path (str): The directory path where the file will be saved. Defaults to '../web_service/local_objects'.
+    """
     os.makedirs(folder_path, exist_ok=True)
-
-    # Full path to save the file
     file_path = os.path.join(folder_path, f"{filename}.pkl")
-
-    # Save the object to the file
-    with open(file_path, 'wb') as f:
-        pickle.dump(obj, f)
-
-    print(f"Object saved to {file_path}")
-
+    try:
+        with open(file_path, 'wb') as f:
+            pickle.dump(obj, f)
+        logger.info(f"Object saved to {file_path}")
+    except Exception as e:
+        logger.error(f"Failed to pickle object: {e}")
 
 @lru_cache
-def load_preprocessor(filepath: os.PathLike):
+def load_preprocessor(filepath: os.PathLike) -> object:
+    """
+    Loads a pickled preprocessor from the specified file path.
+
+    Args:
+        filepath (os.PathLike): The path to the pickled preprocessor file.
+
+    Returns:
+        object: The loaded preprocessor.
+    """
     logger.info(f"Loading preprocessor from {filepath}")
-    with open(filepath, "rb") as f:
-        return pickle.load(f)
-
+    try:
+        with open(filepath, "rb") as f:
+            return pickle.load(f)
+    except Exception as e:
+        logger.error(f"Failed to load preprocessor: {e}")
+        raise
 
 @lru_cache
-def load_model(filepath: os.PathLike):
+def load_model(filepath: os.PathLike) -> object:
+    """
+    Loads a pickled model from the specified file path.
+
+    Args:
+        filepath (os.PathLike): The path to the pickled model file.
+
+    Returns:
+        object: The loaded model.
+    """
     logger.info(f"Loading model from {filepath}")
-    with open(filepath, "rb") as f:
-        return pickle.load(f)
+    try:
+        with open(filepath, "rb") as f:
+            return pickle.load(f)
+    except Exception as e:
+        logger.error(f"Failed to load model: {e}")
+        raise
