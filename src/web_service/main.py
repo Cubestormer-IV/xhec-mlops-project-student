dae68c13a0c33fc2af5a0842ed57d4bccd834777
Cubestormer-IV <tanmay@gmail.com>
2024-10-24 21:20:37 +0200

    Added logic to API main and utils modules

--- src/web_service/main.py
+++ src/web_service/main.py
@@ -2,10 +2,12 @@
 
 
 from fastapi import FastAPI
+from src.web_service.lib.inference import infer_age
+from src.web_service.lib.models import InputSchema, PredictionResponseSchema
 
 # Other imports
 
-app = FastAPI(title="...", description="...")
+app = FastAPI(title="Artefact MLOps Project", description="Very simple API using the ")
 
 
 @app.get("/")
@@ -16,3 +18,6 @@ def home() -> dict:
 @app.post("/predict", response_model=PredictionResponseSchema, status_code=201)
 def predict(payload: InputSchema) -> dict:
     # TODO: complete and replace the "InsertHereAPydanticClass" with the correct Pydantic classes defined in web_service/lib/models.py
+    prediction = infer_age(payload)
+    return {"abalone_age_prediction": y}
+    
--- src/web_service/utils.py
+++ src/web_service/utils.py
@@ -1,1 +1,23 @@
-# Utils file. TODO: add a `load_object` function to load pickle objects
+import os
+import pickle
+from functools import lru_cache
+from loguru import logger
+
+@lru_cache
+def load_object(filepath: os.PathLike) -> object:
+    """
+    Loads a pickled model from the specified file path.
+
+    Args:
+        filepath (os.PathLike): The path to the pickled model file.
+
+    Returns:
+        object: The loaded model.
+    """
+    logger.info(f"Loading model from {filepath}")
+    try:
+        with open(filepath, "rb") as f:
+            return pickle.load(f)
+    except Exception as e:
+        logger.error(f"Failed to load model: {e}")
+        raise
